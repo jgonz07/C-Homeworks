8b0f39a17bc35f5bc0a71f75c19597c51ebcbe23
Sebastian <jsgonz90@gmail.com>
2016-05-17 19:48:46 -0400

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter length of long side");
+            int longside = int.Parse(Console.ReadLine()) ;
+
+            Console.WriteLine("enter length of short side");
+            int shortside = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine("the area of your rectangle would be {0} !", multiply(longside, shortside));
+            Console.ReadLine();
+        }
+
+        public static int multiply (int x,int y)
+        {
+            return (x * y);
+        }
+    }
+}
