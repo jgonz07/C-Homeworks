4a7c5cc89c1a04d8085e81612bdc1b34310f5762
Sebastian <jsgonz90@gmail.com>
2016-05-17 20:24:13 -0400

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number to multiply");
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int j = 1; j <= n; j++)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                   int print= ( i * j);
+                    Console.Write("{0} ", print);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
